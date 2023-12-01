95a8af2a321453cb1e5f781bc6b08f47645f1a05
Shubham Subhnil <shubhamsubhniloptimus@gmail.com>
2023-12-01 19:53:59 +0000

    Baselines tester file
    Used to make Stable-BaseLines3 compatible environments.

--- causal_gridworlds/rl_implementations/A2C-baselines-test.py
+++ causal_gridworlds/rl_implementations/A2C-baselines-test.py
@@ -10,16 +10,17 @@ from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_vec_env
 from custom_envs.BL3_stoch_king_windy_gridworld_env import StochKingWindyGridWorldEnv
 
-env = make_vec_env(StochKingWindyGridWorldEnv, n_envs = 2)
+import wandb
+
+env = make_vec_env(StochKingWindyGridWorldEnv, n_envs=4)
 
 model = A2C("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=2000)
+model.learn(total_timesteps=50000)
 model.save("a2c_stoch_king")
 
-obs = env.reset()
-steps = 0
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    steps += 1
-    print(steps)
+
+
+for i in range(0, 1000):
+    obs = env.reset()
+    done = False
+    while not done:
